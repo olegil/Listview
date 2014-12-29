075b9db717d336f08b0ec0a603df51ae82ccc951
sanjaypaul <sanjaypaul.bd@gmail.com>
2014-12-29 13:40:29 +0600

    new

---
+++ listview test/Form1.Designer.cs
@@ -0,0 +1,162 @@
+﻿namespace listview_test
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.nametextBox = new System.Windows.Forms.TextBox();
+            this.adresstextBox = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.emailtextBox = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.showlistView = new System.Windows.Forms.ListView();
+            this.saveButton = new System.Windows.Forms.Button();
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader3 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(28, 23);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Name";
+            // 
+            // nametextBox
+            // 
+            this.nametextBox.Location = new System.Drawing.Point(143, 20);
+            this.nametextBox.Name = "nametextBox";
+            this.nametextBox.Size = new System.Drawing.Size(100, 20);
+            this.nametextBox.TabIndex = 1;
+            // 
+            // adresstextBox
+            // 
+            this.adresstextBox.Location = new System.Drawing.Point(143, 58);
+            this.adresstextBox.Name = "adresstextBox";
+            this.adresstextBox.Size = new System.Drawing.Size(100, 20);
+            this.adresstextBox.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(28, 65);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(39, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Adress";
+            // 
+            // emailtextBox
+            // 
+            this.emailtextBox.Location = new System.Drawing.Point(156, 114);
+            this.emailtextBox.Name = "emailtextBox";
+            this.emailtextBox.Size = new System.Drawing.Size(100, 20);
+            this.emailtextBox.TabIndex = 5;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(41, 117);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(31, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "email";
+            // 
+            // showlistView
+            // 
+            this.showlistView.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1,
+            this.columnHeader2,
+            this.columnHeader3});
+            this.showlistView.Location = new System.Drawing.Point(36, 193);
+            this.showlistView.Name = "showlistView";
+            this.showlistView.Size = new System.Drawing.Size(320, 100);
+            this.showlistView.TabIndex = 6;
+            this.showlistView.UseCompatibleStateImageBehavior = false;
+            this.showlistView.View = System.Windows.Forms.View.Details;
+            // 
+            // saveButton
+            // 
+            this.saveButton.Location = new System.Drawing.Point(312, 75);
+            this.saveButton.Name = "saveButton";
+            this.saveButton.Size = new System.Drawing.Size(75, 23);
+            this.saveButton.TabIndex = 7;
+            this.saveButton.Text = "save";
+            this.saveButton.UseVisualStyleBackColor = true;
+            this.saveButton.Click += new System.EventHandler(this.saveButton_Click);
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "name";
+            // 
+            // columnHeader2
+            // 
+            this.columnHeader2.Text = "Email";
+            // 
+            // columnHeader3
+            // 
+            this.columnHeader3.Text = "Adress";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(398, 336);
+            this.Controls.Add(this.saveButton);
+            this.Controls.Add(this.showlistView);
+            this.Controls.Add(this.emailtextBox);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.adresstextBox);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.nametextBox);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox nametextBox;
+        private System.Windows.Forms.TextBox adresstextBox;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox emailtextBox;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ListView showlistView;
+        private System.Windows.Forms.Button saveButton;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.ColumnHeader columnHeader2;
+        private System.Windows.Forms.ColumnHeader columnHeader3;
+    }
+}
+
---
+++ listview test/Form1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace listview_test
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void saveButton_Click(object sender, EventArgs e)
+        {
+            string name = nametextBox.Text;
+            string email = emailtextBox.Text;
+            string address = adresstextBox.Text;
+
+            ListViewItem items = new ListViewItem();
+            items.Text = name;
+            items.SubItems.Add(email);
+            items.SubItems.Add(address);
+            showlistView.Items.Add(items);
+
+
+            // List<string> sTudent = new List<string>();
+            // List<ListViewItem> n = new List<ListViewItem>();
+
+
+
+
+        }
+
+        
+    }
+}
